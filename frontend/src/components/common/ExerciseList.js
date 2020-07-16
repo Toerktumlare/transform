fdf1e58856018afe49aac80b33dfd77775ab98f5
Thomas Andolf <thomas.andolf@gmail.com>
2020-07-16 14:34:31 +0200

    removed faulty package.lock file

--- frontend/src/components/common/ExerciseList.js
+++ frontend/src/components/common/ExerciseList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Box from '@material-ui/core/box'
+import Box from '@material-ui/core/Box'
 import List from '@material-ui/core/List'
 import Divider from '@material-ui/core/Divider'
 import ListItem from '@material-ui/core/ListItem'
