7026fc1ab9121b17aff475019da2d93f8217ce39
Thomas Andolf <thomas.andolf@gmail.com>
2020-07-18 18:06:54 +0200

    added metrics and debugging information

--- src/main/java/se/andolf/transform/TransformApplication.java
+++ src/main/java/se/andolf/transform/TransformApplication.java
@@ -2,11 +2,15 @@ package se.andolf.transform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.scheduler.Schedulers;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class TransformApplication {
 
 	public static void main(String[] args) {
+        ReactorDebugAgent.init();
+        Schedulers.enableMetrics();
 		SpringApplication.run(TransformApplication.class, args);
 	}
 
